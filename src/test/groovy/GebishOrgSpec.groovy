086fb08c7616f3ac8c9c4f41937547a53b6a86ce
PoohSunny <swimming.pooh0902@gmail.com>
2016-07-30 11:22:13 +0900

    課題用にテストの中身を少し修正

--- src/test/groovy/GebishOrgSpec.groovy
+++ src/test/groovy/GebishOrgSpec.groovy
@@ -3,10 +3,18 @@ import geb.spock.GebReportingSpec
 class GebishOrgSpec extends GebReportingSpec {
 
     def "Gebのホームページが表示できる"() {
-        when: "Gebのホームページに行くと"
+        given: "Gebのホームページにいて"
         go "http://www.gebish.org/"
 
-        then: "タイトルは'Geb - Very Groovy Browser Automation'であるべき"
-        title == "Very Groovy Browser Automation!"
+        when: "マニュアルのメニューのところにホバーして"
+        interact {
+            moveToElement($("#header-content ul li", 0).children("span"))
+        }
+
+        and: "最初のリンクをクリックすると"
+        $("#header-content ul li", 0).$('.link-list li a')[0].click()
+
+        then: "アニュアルページに遷移する"
+        title == "The Book Of Geb"
     }
 }
