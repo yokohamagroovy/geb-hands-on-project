421d10b065293f51802e7533c4a387fd8f6084df
PoohSunny <swimming.pooh0902@gmail.com>
2016-07-30 11:39:25 +0900

    解答を作成

--- src/test/groovy/GebishOrgSpec.groovy
+++ src/test/groovy/GebishOrgSpec.groovy
@@ -2,13 +2,17 @@ import geb.spock.GebReportingSpec
 
 class GebishOrgSpec extends GebReportingSpec {
 
-    def "Gebのマニュアルページが表示できる"() {
+    def "Gebのホームページが表示できる"() {
         given: "Gebのホームページにいて"
         go "http://www.gebish.org/"
 
         when: "マニュアルのメニューのところにホバーして"
+        interact {
+            moveToElement($("#header-content ul li", 0).children("span"))
+        }
 
         and: "最初のリンクをクリックすると"
+        $("#header-content ul li", 0).$('.link-list li a')[0].click()
 
         then: "アニュアルページに遷移する"
         title == "The Book Of Geb"
