51b92da1448d6f9d9427e535e95c3e0dd97ba5b1
PoohSunny <swimming.pooh0902@gmail.com>
2016-07-30 11:28:32 +0900

    課題用にテスト内容を少し修正

--- src/test/groovy/GebishOrgSpec.groovy
+++ src/test/groovy/GebishOrgSpec.groovy
@@ -2,11 +2,15 @@ import geb.spock.GebReportingSpec
 
 class GebishOrgSpec extends GebReportingSpec {
 
-    def "Gebのホームページが表示できる"() {
-        when: "Gebのホームページに行くと"
+    def "Gebのマニュアルページが表示できる"() {
+        given: "Gebのホームページにいて"
         go "http://www.gebish.org/"
 
-        then: "タイトルは'Geb - Very Groovy Browser Automation'であるべき"
-        title == "Very Groovy Browser Automation!"
+        when: "マニュアルのメニューのところにホバーして"
+
+        and: "最初のリンクをクリックすると"
+
+        then: "アニュアルページに遷移する"
+        title == "The Book Of Geb"
     }
 }
